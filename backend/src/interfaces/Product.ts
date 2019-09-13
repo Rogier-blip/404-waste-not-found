1a1fdb4fba0d8839aa493a63d374638db9ae0f17
Panagiotis Ganelis <panagiotis.ganelis-cic.netherlands@ibm.com>
2019-09-13 17:32:44 +0200

    please elaborate

--- backend/src/interfaces/Product.ts
+++
@@ -1,7 +0,0 @@
-export interface Product {
-  name: String;
-  gronies: Number;
-  description: String;
-  img: String;
-  barcode: Number;
-}
--- backend/src/models/user.ts
+++
@@ -1,9 +0,0 @@
-import { IUser } from "../interfaces/IUser";
-
-export class User implements IUser {
-  constructor(
-    public firstName: string,
-    public lastName: string,
-    public gronies: Number
-  ) {}
-}
