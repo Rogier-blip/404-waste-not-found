ee082fe2329ef0fc7eb6112d3440b713401eb056
Panagiotis Ganelis <panagiotis.ganelis-cic.netherlands@ibm.com>
2019-09-13 14:19:23 +0200

    Create interface for user and product and two rest apis requests

--- backend/src/controllers/user.controller.ts
+++ backend/src/controllers/user.controller.ts
@@ -1,16 +1,24 @@
-import { Request, Response, Router } from 'express';
+import { Request, Response, Router } from "express";
 
-import { User } from '../models/user';
+import { User } from "../models/user";
 
 const userController: Router = Router();
 
-userController.get('/', (req: Request, res: Response): void => {
-    res.send(`You requested GET /users`);
+userController.get("/", (req: Request, res: Response): void => {
+  res.send(`You requested GET /users`);
 });
 
-userController.post('/', (req: Request, res: Response): void => {
-    const user = new User(req.body.email)
-    res.send(user)
+userController.post("/updateUser", (req: Request, res: Response): void => {
+  res.send("Thanks for the update man <3");
+});
+
+userController.post("/", (req: Request, res: Response): void => {
+  const user = new User(
+    req.body.firstName,
+    req.body.lastName,
+    req.body.gronies
+  );
+  res.send(user);
 });
 
 module.exports = userController;
